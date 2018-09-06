445951e805db3cf2817be16519d6f19680dde926
Anne <anais.kare@gmail.com>
2018-09-06 10:14:09 -0300

    AGREGAR -> practice

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: any[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public userService: UserServiceProvider
+  ) {}
+
+  ionViewDidLoad(){
+    this.userService.getUsers()
+    .subscribe(
+      (data) => { // Success
+        this.users = data['results'];
+        console.log(this.users)
+      },
+      (error) =>{
+        console.error(error);
+      }
+    )
 
   }
 
